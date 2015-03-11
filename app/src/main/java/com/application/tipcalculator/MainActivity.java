66c22c5672910922eb690a5d73dec34a64924b1c
24digital <24digital@gmail.com>
2015-03-11 11:19:40 -0400

    Completed

--- app/src/main/java/com/application/tipcalculator/MainActivity.java
+++ app/src/main/java/com/application/tipcalculator/MainActivity.java
@@ -10,6 +10,8 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 
 public class MainActivity extends ActionBarActivity {
 private RadioGroup buttonGroup;
@@ -20,6 +22,11 @@ private RadioGroup buttonGroup;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        buttonGroup = (RadioGroup) findViewById(R.id.buttonGroup);
+        userBill = (EditText) findViewById(R.id.bill_Form);
+        tipAmount = (TextView) findViewById(R.id.tipValue);
+        totalAmount = (TextView) findViewById(R.id.totalValue);
     }
 
 
@@ -46,15 +53,18 @@ private RadioGroup buttonGroup;
     }
     public void onClickFindAmount(View view) {
 
-
         buttonGroup = (RadioGroup) findViewById(R.id.buttonGroup);
         userBill = (EditText) findViewById(R.id.bill_Form);
-        String temp = userBill.getText().toString();
-        double userBillAmnt = Double.parseDouble(temp);
-        int i = buttonGroup.getCheckedRadioButtonId();
-
         tipAmount = (TextView) findViewById(R.id.tipValue);
         totalAmount = (TextView) findViewById(R.id.totalValue);
+
+        String temp = userBill.getText().toString();
+     NumberFormat format = java.text.NumberFormat.getCurrencyInstance();
+
+        double userBillAmnt = Double.parseDouble(temp);
+
+
+
         Double tipTemp = 0.0;
         Double totalTemp = 0.0;
 
@@ -62,36 +72,45 @@ private RadioGroup buttonGroup;
             boolean checked = ((RadioButton)view).isChecked();
         switch (view.getId())
         {
+
             case R.id.ten:
                 tipTemp =  userBillAmnt * .10;
                 totalTemp = userBillAmnt + tipTemp;
 
                 totalTemp = castRoundTo2(totalTemp);
-                tipAmount.setText(String.valueOf(tipTemp));
-                totalAmount.setText(String.valueOf(totalTemp));
+                tipAmount.setText(format.format(tipTemp));
+
+                totalAmount.setText(format.format(totalTemp));
+                userBill.setText(format.format(userBillAmnt));
                 break;
             case R.id.eighteen:    tipTemp = userBillAmnt * .18;
                 totalTemp = userBillAmnt + tipTemp;
 
                 totalTemp = castRoundTo2(totalTemp);
-                tipAmount.setText(String.valueOf(tipTemp));
-                totalAmount.setText(String.valueOf(totalTemp));
+                tipAmount.setText(format.format(tipTemp));
+
+                totalAmount.setText(format.format(totalTemp));
+                userBill.setText(format.format(userBillAmnt));
                 break;
             case R.id.twenty:
                 tipTemp = userBillAmnt * .20;
                 totalTemp = userBillAmnt + tipTemp;
 
                 totalTemp = castRoundTo2(totalTemp);
-                tipAmount.setText(String.valueOf(tipTemp));
-                totalAmount.setText(String.valueOf(totalTemp));
+                tipAmount.setText(format.format(tipTemp));
+
+                totalAmount.setText(format.format(totalTemp));
+                userBill.setText(format.format(userBillAmnt));
                 break;
             case R.id.fifteen:
                 tipTemp = userBillAmnt * .15;
                 totalTemp = userBillAmnt + tipTemp;
 
                 totalTemp = castRoundTo2(totalTemp);
-                tipAmount.setText(String.valueOf(tipTemp));
-                totalAmount.setText(String.valueOf(totalTemp));
+                tipAmount.setText(format.format(tipTemp));
+
+                totalAmount.setText(format.format(totalTemp));
+                userBill.setText(format.format(userBillAmnt));
                 break;
 
         }
@@ -102,15 +121,24 @@ private RadioGroup buttonGroup;
     @Override
     protected void onRestoreInstanceState(Bundle savedInstanceState) {
         super.onRestoreInstanceState(savedInstanceState);
-   //     CalculatorEngine engine = (CalculatorEngine) savedInstanceState.getSerializable("obj");
-   //     if (engine != null)
-       //     calculatorEngine = engine;
+        buttonGroup = (RadioGroup) findViewById(R.id.buttonGroup);
+        userBill = (EditText) findViewById(R.id.bill_Form);
+        tipAmount = (TextView) findViewById(R.id.tipValue);
+        totalAmount = (TextView) findViewById(R.id.totalValue);
+
+
+        tipAmount.setText((String) savedInstanceState.get("tip"));
+
+        totalAmount.setText((String)savedInstanceState.get("total"));
+           userBill.setText((String)savedInstanceState.get("bill"));
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-    //    outState.putSerializable("obj", calculatorEngine);
+          outState.putString("tip",tipAmount.getText().toString());
+        outState.putString("total", totalAmount.getText().toString());
+        outState.putString("bill", userBill.getText().toString());
     }
         public  double castRoundTo2(double d) {
         return (long) (d * 100 + 0.5) / 100.0;
